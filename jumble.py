9dc748fd7919b768851c1dd7bb6b2a6f57e6a37a
Eric Ren <rencire@gmail.com>
2014-03-29 21:37:38 -0700

    added functionality to invoke solver through command line

--- jumble.py
+++ jumble.py
@@ -1,16 +1,18 @@
-def solve(word):
-    # generate english dictionary
+import argparse
+
+
+def solve(word, file):
+    """Generates a set of words that can be created from input word"""
     words = set()
     ref_counts = get_char_counts(word)
 
-    f = open('./my_6of12.txt', 'r')
+    f = open(file, 'r')
     for line in f:
         #remove \n character
         w = line[:-1]
         # assume we ignore case
         w = w.lower()
         char_counts = get_char_counts(w)
-        # print(char_counts)
         w_is_subset = True
         for char, count in char_counts.items():
             if char not in ref_counts or count > ref_counts[char]:
@@ -31,5 +33,20 @@ def get_char_counts(string):
         else:
             counts[char] = 1
     return counts
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A word jumble solver.  Prints a list of words that can be formed from the input word.')
+    parser.add_argument('word', help='Input string for word jumble')
+    parser.add_argument('-d', '--dictionary', help='Use a different dictionary file other than the default.  Dictionary file  must contain one word per line or else unexpected results might happen.')
+    args = parser.parse_args()
+    
+    file = './my_6of12_dict.txt'
+    if args.dictionary:
+        file = args.dictionary
+
+    words = solve(args.word, file)
+    for w in words:
+        print(w)
 
 
